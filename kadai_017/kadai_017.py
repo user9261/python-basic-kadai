0a6288238ca5b91a32e9429ea8dc29008cf1e661
user9261 <nre12270@gmail.com>
2024-03-28 05:06:41 +0000

    kadai_017

--- kadai_017/kadai_017.py
+++ kadai_017/kadai_017.py
@@ -4,7 +4,7 @@ class Human:
         self.name = name
         self.age = age
     
-    # printinfoメソッドを定義   
+    # check_adultメソッドを定義   
     def check_adult(self):
       if self.age >= 20:
            print(f"{self.name}は大人です。")
@@ -18,7 +18,7 @@ human3 = Human("侍次郎", 20)
 human4 = Human("侍花子", 18)
 humans = [human1, human2, human3, human4]
 
-#インスタンス単体に対し、check_adultを起動
+#リストの要素数分だけcheck_adultメソッドを呼び出して出力
 for human in humans:
   human.check_adult()  
 
